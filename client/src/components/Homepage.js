5f2f127e107263a514a123d73c81516ee679019b
Kurt Waizmann <kurtwaizmann@gmail.com>
2022-08-09 11:51:44 -0400

    homepage — good chunk of the animation done

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -59,10 +59,38 @@ const Homepage = () => {
       x: 0,
       opacity:1,
       transition: {
-        duration:1
+        duration:1.5
       }
     }
   }
+
+  const animateCEOText = {
+    start:{
+      opacity: 0,
+    },
+    end: {
+      opacity: 1,
+      transition: {
+        type: "tween",
+        duration:1.5,
+        delay: 0.5,
+      }
+    }
+  }
+  const animateImageLeft = {
+    start:{
+      x: -400,
+      opacity:0
+    },
+    end: {
+      x: 0,
+      opacity:1,
+      transition: {
+        duration:1.5
+      }
+    }
+  }
+
 
   return (
     <Body>
@@ -104,17 +132,25 @@ const Homepage = () => {
       {/* third fold — CEO */}
       <FoldThree>
         <WrapperCeo>
+        <CEOText
+          as={motion.div}  
+          initial={"start"}
+          whileInView={"end"}
+          viewport={{once:true, amount:0.8}}
+          variants={animateCEOText}
+        >
         <Leader>Our Leader</Leader>
         <InnerWrapCEO>
           <TitleCEO>From the spark of a glorious mind is born a revolution</TitleCEO>
           <DecriptionTwo>From humble beginnings, TrueNeural CEO Alan Rowe gathered the best people in the industry to build a new revolutionary technology. Others had deemed it impossible, but he chose to invest his own life to create a new and better one for others to enjoy. As luck would have it, he succeeded.</DecriptionTwo>
           <More>Find out more</More>
         </InnerWrapCEO>
+        </CEOText>
         <ImageTwo src={imageCEO}
           as={motion.img}  
           initial={"start"}
           whileInView={"end"}
-          viewport={{once:false, amount:0.1}}
+          viewport={{once:true, amount:0.35}}
           variants={animateCEO}
         />
         </WrapperCeo>
@@ -128,16 +164,31 @@ const Homepage = () => {
 
       {/* fifth fold — three pillars */}
       <FoldFive>
-      <WrapperPillars>
-        <Pillar>
+      <WrapperPillars
+      as={motion.div}
+      initial={"start"}
+      whileInView={"end"}
+      viewport={{once:true, amount:0.8}}
+      transition={{staggerChildren:0}}
+      >
+        <Pillar
+          as={motion.div}  
+          variants={animateCEOText}
+        >
           <PillarName>Positivity</PillarName>
           <PillarDescription>Structured to minimize negative emotion and instead promote positivity and happiness.</PillarDescription>
         </Pillar>
-        <Pillar>
+        <Pillar
+          as={motion.div}  
+          variants={animateCEOText}
+        >
           <PillarName>Meaning</PillarName>
           <PillarDescription>Algoriths to monitor and impart insightful meaning to every piece of your new existance.</PillarDescription>
         </Pillar>
-        <Pillar>
+        <Pillar
+          as={motion.div}  
+          variants={animateCEOText}
+        >
           <PillarName>Freedom</PillarName>
           <PillarDescription>Boundaries are broken with a world that’s yours to fully explore and create.</PillarDescription>
         </Pillar>
@@ -153,27 +204,54 @@ const Homepage = () => {
       {/* seven fold — two employees */}
       <FoldSeven>
         <ContainerMind>
-          <WrapperMind>
+          <WrapperMind
+            as={motion.div}  
+            initial={"start"}
+            whileInView={"end"}
+            viewport={{once:true, amount:0.8}}
+            variants={animateCEOText}
+          >
             <TitleMind>Your mind is in the best of hands</TitleMind>
             <DescriptionMind>Let us be your guide through the transition into a new life. Our employees, who we consider family, will be with you every step of the process. They will help you settled into the right spiritual mindset before initializing the upload.</DescriptionMind>
             <More>Find out more</More>
           </WrapperMind>
-          <ImageEmployees src={imageEmployees} />
+          <ImageEmployees src={imageEmployees} 
+            as={motion.img}  
+            initial={"start"}
+            whileInView={"end"}
+            viewport={{once:true, amount:0.35}}
+            variants={animateCEO}
+          />
         </ContainerMind>
       </FoldSeven>
 
       {/* eigth fold — three more pillars */}
       <FoldEight>
-      <WrapperPillars>
-        <Pillar>
+      <WrapperPillars
+        as={motion.div}
+        initial={"start"}
+        whileInView={"end"}
+        viewport={{once:true, amount:0.8}}
+        transition={{staggerChildren:0}}
+      >
+        <Pillar
+          as={motion.div}  
+          variants={animateCEOText}
+        >
           <PillarName>Belonging</PillarName>
           <PillarDescription>Develop profound relationships with other avatars who will value you for you.</PillarDescription>
         </Pillar>
-        <Pillar>
+        <Pillar
+          as={motion.div}  
+          variants={animateCEOText}
+        >
           <PillarName>Transcendence</PillarName>
           <PillarDescription>Break apart the old paradigms of what it means to live a life, and follow ever life paths without fear or regret.</PillarDescription>
         </Pillar>
-        <Pillar>
+        <Pillar
+          as={motion.div}  
+          variants={animateCEOText}
+        >
           <PillarName>Storytelling</PillarName>
           <PillarDescription>We are the authors of our lives, so create the narrative that brings clarity and reenforces your purpose.</PillarDescription>
         </Pillar>
@@ -182,27 +260,66 @@ const Homepage = () => {
 
       {/* ninth fold — customer going to sleep */}
       <FoldNine>
-        <img src={imageSleeper} />
-        <SloganDream>Drift into a Lovely Dream</SloganDream>
+        <motion.img src={imageSleeper} 
+          initial={"start"}
+          whileInView={"end"}
+          viewport={{once:true, amount:0.35}}
+          variants={animateImageLeft}
+        />
+        <SloganDream
+          as={motion.h1}  
+          initial={"start"}
+          whileInView={"end"}
+          viewport={{once:true, amount:0.8}}
+          variants={animateCEOText}
+        >Drift into a Lovely Dream</SloganDream>
       </FoldNine>
 
       {/* tenth fold — employee assisting customer */}
       <FoldTen>
-        <img src={imageSleepAssist} />
+        <motion.img src={imageSleepAssist} 
+          initial={"start"}
+          whileInView={"end"}
+          viewport={{once:true, amount:0.35}}
+          variants={animateImageLeft}
+        
+        />
       </FoldTen>
 
       <FoldEleven>
-        <WrapTestimonial>
+        <WrapTestimonial
+          as={motion.div}  
+          initial={"start"}
+          whileInView={"end"}
+          viewport={{once:true, amount:0.8}}
+          variants={animateCEOText}
+        >
         <Stars>★★★★★</Stars>
           <Testimonial>At the age of 78, I signed up to After.  I was quite resistant at first. But my son wore me out. I guess I liked this option better than being sent into a retirement home. And thank god for my son’s insistance. Now I’m 50 years younger, verile, and full of energy. And after seeing all the fun I was having, my wife soon joined and she’s now in her 20s! What a georgeous thing.</Testimonial>
           <Signed>Richie Guzman — Uploaded in 2021</Signed>
         </WrapTestimonial>
-        <img src={imageTestimonail1} />
+        <motion.img src={imageTestimonail1}  
+          initial={"start"}
+          whileInView={"end"}
+          viewport={{once:true, amount:0.35}}
+          variants={animateCEO}
+        />
       </FoldEleven>
 
       <FoldTwelve>
-        <img src={imageTestimonail2} />
-        <WrapTestimonial>
+        <motion.img src={imageTestimonail2}   
+          initial={"start"}
+          whileInView={"end"}
+          viewport={{once:true, amount:0.35}}
+          variants={animateImageLeft}
+        />
+        <WrapTestimonial
+          as={motion.div}  
+          initial={"start"}
+          whileInView={"end"}
+          viewport={{once:true, amount:0.8}}
+          variants={animateCEOText}
+        >
         <Stars>★★★★★</Stars>
           <Testimonial>I was dying from cancer with only a few months to live when I discovered After. What a miracle. I’ve left my diseased body behind for everlasting vitality. I only wish I could get my loved ones to join me. I guess they will someday :)</Testimonial>
           <Signed>Jordan Hutchinson — Uploaded in 2020</Signed>
@@ -210,17 +327,39 @@ const Homepage = () => {
       </FoldTwelve>
 
       <FoldThirteen>
-        <WrapTestimonial>
+        <WrapTestimonial
+          as={motion.div}  
+          initial={"start"}
+          whileInView={"end"}
+          viewport={{once:true, amount:0.8}}
+          variants={animateCEOText}
+        >
         <Stars>★★★★★</Stars>
           <Testimonial>Honestly, I was so fortunate to be able leave that so-called “real” world. Let me remind you, a world ravaged by global warming, economic crises, endless wars, and so much more hurt. I remember struggling for simple things like finding fresh fruit when all the groceries stores were empty, or hiding indoors from the yearly pandemic. In the After, it’s been nothing but beautiful weather and happy moments. Truly a paradise! </Testimonial>
           <Signed> Suzette Aguilar — Uploaded in 2022</Signed>
         </WrapTestimonial>
-        <img src={imageTestimonail3} />
+        <motion.img src={imageTestimonail3}   
+          initial={"start"}
+          whileInView={"end"}
+          viewport={{once:true, amount:0.35}}
+          variants={animateCEO}
+        />
       </FoldThirteen>
 
       <FoldFourteen>
-        <img src={imageTestimonail4} /> 
-        <WrapTestimonial>
+        <motion.img src={imageTestimonail4}   
+          initial={"start"}
+          whileInView={"end"}
+          viewport={{once:true, amount:0.35}}
+          variants={animateImageLeft}
+        /> 
+        <WrapTestimonial
+          as={motion.div}  
+          initial={"start"}
+          whileInView={"end"}
+          viewport={{once:true, amount:0.8}}
+          variants={animateCEOText}
+        >
           <Stars>★★★★★</Stars>
           <Testimonial>After getting married, me and my wife moved into After. Sadly, we divorced within the year as my wife had trouble adapting. But there’s love all around! I was even able to make the woman of my dreams, with all the best features  and a wonderful personality. And let me tell you, Kiri is as real as any woman! We just connect on such a level that me and my ex-wife (rip) never could. I thank every day that I got on the path that led me to my precious Kiri...</Testimonial>
           <Signed>Caleb Pierce — Uploaded in 2020</Signed>
@@ -229,9 +368,16 @@ const Homepage = () => {
 
 
       <FoldFifteen>
-        <ImageBoy style ={ { backgroundImage: `url(${imageBoy})` } }/>
-        <Forever>Preserve the people that are dearest to you, forever</Forever>
-        <Button>UPLOAD NOW</Button>
+        <ImageBoy style ={ { backgroundImage: `url(${imageBoy})` } }>
+          <Forever>Preserve the people that are dearest to you, forever</Forever>
+          <Button
+            as={motion.button}  
+            initial={"start"}
+            whileInView={"end"}
+            viewport={{once:true, amount:0.8}}
+            variants={animateCEOText}
+          >UPLOAD NOW</Button>
+        </ImageBoy>
       </FoldFifteen>
 
       <FoldSixteen>
@@ -341,15 +487,19 @@ const WrapperCeo = styled.div`
   width: 100%;
   gap:8%;
 `
+const CEOText = styled.div`
+  display: flex;
+  gap:8%;
+`
 const Leader = styled.div`
-font-size: 24px;
-font-weight: 600;
-line-height: 33px;
-letter-spacing: 0em;
-text-align: left;
-color: #6D6466;
-display: flex;
-width: 85px;
+  font-size: 24px;
+  font-weight: 600;
+  line-height: 33px;
+  letter-spacing: 0em;
+  text-align: left;
+  color: #6D6466;
+  display: flex;
+  width: 85px;
 `
 const InnerWrapCEO = styled.div`
   width: 518px;
@@ -485,8 +635,7 @@ const ContainerMind = styled.div`
   justify-content: space-between;
   align-items: center;
   background-color: #F1F0EA;
-  padding: 0 15% 0 7%;
-  gap: 20%;
+  padding: 0 15% 0 15%;
 `
 const WrapperMind = styled.div`
   width: 490px;
@@ -636,9 +785,14 @@ const ImageBoy = styled.div`
   height: 637px;
   width: 100%;
   border: 2px solid red;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: left;
+  gap: 50px;
+  padding-left: 100px;
 `
 const Forever = styled.div`
-  position: absolute;
   width: 389px;
   font-size: 40px;
   font-weight: 700;
@@ -646,7 +800,6 @@ const Forever = styled.div`
   letter-spacing: 0em;
   text-align: left;
   color: #545F75;
-  left: 70px;
 `
 const Button = styled.div`
   height: 61px;
@@ -663,9 +816,6 @@ const Button = styled.div`
   font-variant: small-caps;
   color: #FFFFFF;
   background-color: #545F75;
-  position: absolute;
-  left: 70px;
-  top: 500px;
 `
 // sixteenth fold
 const FoldSixteen = styled.div`
